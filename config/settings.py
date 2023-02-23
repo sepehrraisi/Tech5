de9709637638a01e79460bff9f06e90f951a9689
Sara Adibi <sara.adibi2000@yahoo.com>
2023-02-23 15:39:56 +0330

    Setting fix issue

--- config/settings.py
+++ config/settings.py
@@ -133,11 +133,11 @@ STATICFILES_DIRS = [
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = 'static3'
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = 'media'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
